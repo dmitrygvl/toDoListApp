39dc1b53bac4c021b8d182981962d57aeab1001e
dmitrygvl <dmitrygv1994@ya.ru>
2023-12-26 19:01:12 +0300

    Add husky & stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['stylelint-config-standard-scss'],
+  plugins: ['stylelint-prettier', 'stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'scss/at-mixin-pattern': null,
+    'prettier/prettier': true,
+    'no-invalid-position-at-import-rule': null,
+    'selector-class-pattern': null,
+    'no-descending-specificity': null,
+  },
+};
